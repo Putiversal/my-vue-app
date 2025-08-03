7e276cc4bb294b3094f39106d87e1ea44ae714ef
Putiversal <danielfink2001@mail.ru>
2025-08-03 14:30:52 +0300

    feat(TaskManagerView): Added TM and few stiles

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+// import AboutView from "../views/AboutView.vue";
+import TaskManagerView from "../views/TaskManagerView.vue";
 
 const routes = [
   {
@@ -15,6 +17,13 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/taskManager",
+    name: "taskManager",
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/TaskManagerView.vue"),
+    component: TaskManagerView,
   },
 ];
 
---
+++ src/views/TaskManagerView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mainBox">
+    <p>
+      Я хочу сделать список задач которые я буду делать в рамках этого проекта.
+      Так, первой задачей будет менеджер тасок. Доступ к нему можно будет
+      получить на панели слева.
+    </p>
+    <ul>
+      <li>Создать компонент менеджера задач</li>
+      <li>Добавить возможность добавлять новые задачи</li>
+      <li>Реализовать удаление задач</li>
+      <li>Добавить возможность отмечать задачи как выполненные</li>
+      <li>Сохранение задач в localStorage</li>
+      <li>Сделать красивый интерфейс для списка задач</li>
+      <li>Добавить фильтрацию задач (все/выполненные/активные)</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.mainBox {
+  background: #fff;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.08);
+  padding: 32px 24px;
+  margin: 24px auto;
+  max-width: 600px;
+  font-family: "Segoe UI", Arial, sans-serif;
+  color: #222;
+}
+
+.mainBox ul {
+  padding-left: 20px;
+}
+
+.mainBox li {
+  margin-bottom: 8px;
+  line-height: 1.6;
+}
+</style>
