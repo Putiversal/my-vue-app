0c60d6b2b24b4c6380d66b1f7637a111f0d34c3b
Putiversal <danielfink2001@mail.ru>
2025-08-07 17:25:13 +0300

    feat(): Added vuetify and few features in About Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-// import AboutView from "../views/AboutView.vue";
+import AboutView from "../views/AboutView.vue";
 import TaskManagerView from "../views/TaskManagerView.vue";
 
 const routes = [
@@ -15,8 +15,9 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
   },
   {
     path: "/taskManager",
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,38 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <button @click="incrementCounter">Simple counter: {{ counter }}</button>
+  <button @click="resetCounter">Reset counter</button>
+  <p :class="isRed">This text is red when counter is 2</p>
+  <br />
 </template>
+
+<script setup>
+import { ref, watch } from "vue";
+
+const counter = ref(0);
+
+const isRed = ref("");
+
+const incrementCounter = () => {
+  counter.value++;
+};
+
+const resetCounter = () => {
+  counter.value = 0;
+};
+
+watch(counter, (newValue) => {
+  if (newValue == 2) {
+    isRed.value = "red-colored-text";
+  } else {
+    isRed.value = "";
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.red-colored-text {
+  color: red;
+}
+</style>
