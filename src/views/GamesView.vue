af9b4f1fce65d1005ab59f6d4a7b87aaf14eb33c
Putiversal <danielfink2001@mail.ru>
2025-08-12 03:04:16 +0300

    fix(): made snake game pic actual

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -4,7 +4,7 @@
       <v-card class="box-content" to="/games/snake-game"
         ><v-card-title>Змейка</v-card-title>
         <v-card-subtitle>by: Azaz</v-card-subtitle>
-        <v-card-text><img src="@/assets/snakeGamePng.png" /></v-card-text>
+        <v-card-text><img src="@/assets/snakeGameScreen.png" /></v-card-text>
       </v-card>
       <v-card class="box-content"
         ><v-card-title>Игра 2</v-card-title> <v-card-text></v-card-text
@@ -37,9 +37,11 @@ const route = useRoute();
   border-style: solid;
   border-color: black;
   width: 24%;
-  /* height: 80px; */
+  /* height: 240px; */
   margin: 6px;
 }
-.v-subtitle {
+.game-view {
+  padding: 20px;
+  text-align: center;
 }
 </style>
